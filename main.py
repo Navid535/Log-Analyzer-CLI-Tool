863e6b198fadf39aff3faa7bb04aace0edcf7649
Navid535 <navidhoseinzade3@gmail.com>
2026-07-10 19:38:00 +0330

    showing hourly distribution chart in terminal

--- main.py
+++ main.py
@@ -26,6 +26,23 @@ LOG_PATTERN = re.compile(
 
 UNIQUE_IPs = set()
 endpoint_counter = Counter()
+hourly_counter = Counter()
+
+
+def print_histogram(hours):
+    max_value = max(hours.values())
+    max_length = 50
+
+    print(f'{bcolors.HEADER}Hourly Traffic Distribution:{bcolors.ENDC}')
+
+    for h in range(24):
+        hour_str = f"{h:02d}"
+        count = hours.get(hour_str, 0)
+        bar_length = int((count / max_value) * max_length) if max_value > 0 else 0
+        bar = "█" * bar_length
+
+        print(f'Hour {hour_str}: {count:6d} | {bar}')
+
 
 def parse_log_file(file_path):
     all_file_lines = 0
@@ -37,18 +54,27 @@ def parse_log_file(file_path):
         for line in f:
             line = line.strip()
 
-            if not line:
+            if not line:   # empty line
                 all_file_lines += 1
                 continue
 
-            match = LOG_PATTERN.match(line)
+            match = LOG_PATTERN.match(line) # compare with regex
+
             if match:
                 all_file_lines += 1
                 total += 1
+
                 UNIQUE_IPs.add(match.groupdict()['ip'])
                 endpoint_counter[match.groupdict()['path']] += 1
+
+                # 'timestamp': '01/Jun/2026:09:35:50 +0000'
+                hour = match.groupdict()['timestamp'].split(':')[1]
+                hourly_counter[hour] += 1
+
+                # Requests with 4xx or 5xx status code
                 if match.groupdict()['status'].startswith('4') or match.groupdict()['status'].startswith('5'):
                     error_counter += 1
+
 
 
             else:
@@ -63,7 +89,9 @@ def parse_log_file(file_path):
         for e in endpoint_counter.most_common(10):
             print(f'{bcolors.OKGREEN}endpoint: "{e[0]}" occurs {e[1]} times{bcolors.ENDC}')
         print()
-        print(f'{bcolors.FAIL}Errors percent (4XX or 5XX): {round(error_counter / total * 100, 3)}%{bcolors.ENDC}')
+        print(f'{bcolors.FAIL}Errors percent (4XX or 5XX): {round(error_counter / total * 100, 3)}%{bcolors.ENDC}\n')
+        print_histogram(hourly_counter)
 
 if __name__ == '__main__':
-    parse_log_file("access.log/access.log")
+    print(sys.argv)
+    # parse_log_file("access.log/access.log")
