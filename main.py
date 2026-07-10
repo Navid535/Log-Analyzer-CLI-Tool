cd64f59320f2f7ddc65daa3f0ea58f72038093e3
Navid535 <navidhoseinzade3@gmail.com>
2026-07-10 19:06:49 +0330

    basic report: all requests, unique IPs, enpoints top 10, error rate

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import sys, re
+from collections import Counter
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 
 LOG_PATTERN = re.compile(
@@ -7,13 +19,19 @@ LOG_PATTERN = re.compile(
     r'\[(?P<timestamp>[^\]]+)\]\s+'
     r'"(?P<method>\S+)\s+(?P<path>\S+)\s+(?P<protocol>[^"]+)"\s+'
     r'(?P<status>\d+)\s+'
-    r'(?P<size>\S+)'
+    r'(?P<size>\S+)\s+'
+    r'"(?P<referrer>[^"]*)"\s+'
+    r'"(?P<user_agent>[^"]*)"'
 )
+
+UNIQUE_IPs = set()
+endpoint_counter = Counter()
 
 def parse_log_file(file_path):
     all_file_lines = 0
-    total_logs = 0
+    total = 0
     corrupted = 0
+    error_counter = 0
 
     with open(file_path, 'r') as f:
         for line in f:
@@ -26,17 +44,26 @@ def parse_log_file(file_path):
             match = LOG_PATTERN.match(line)
             if match:
                 all_file_lines += 1
-                total_logs += 1
-                # if total_logs == 12:
-                #     print(match.groupdict())
+                total += 1
+                UNIQUE_IPs.add(match.groupdict()['ip'])
+                endpoint_counter[match.groupdict()['path']] += 1
+                if match.groupdict()['status'].startswith('4') or match.groupdict()['status'].startswith('5'):
+                    error_counter += 1
+
+
             else:
                 all_file_lines += 1
-                total_logs += 1
                 corrupted += 1
 
-        print(f'Corrupted: {corrupted}')
-        print(f'Total: {total_logs}')
-        print(f'Corrupted percent: {round((corrupted / total_logs) * 100, 3)}%')
+        print(f'{bcolors.HEADER}Unique IPs: {len(UNIQUE_IPs)}{bcolors.ENDC}\n')
+        print(f'{bcolors.OKBLUE}Total fine requests: {total}{bcolors.ENDC}')
+        print(f'{bcolors.FAIL}Corrupted: {corrupted}{bcolors.ENDC}')
+        print(f'{bcolors.FAIL}Corrupted percent: {round((corrupted / (total + corrupted)) * 100, 3)} %{bcolors.ENDC}\n')
+        print(f'{bcolors.HEADER}Top 10 of most common endpoints:')
+        for e in endpoint_counter.most_common(10):
+            print(f'{bcolors.OKGREEN}endpoint: "{e[0]}" occurs {e[1]} times{bcolors.ENDC}')
+        print()
+        print(f'{bcolors.FAIL}Errors percent (4XX or 5XX): {round(error_counter / total * 100, 3)}%{bcolors.ENDC}')
 
 if __name__ == '__main__':
     parse_log_file("access.log/access.log")
